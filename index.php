7b4cffc7ccae270c8833befb4af7f4b9f069812c
darkwoolf <woolftreuser@yandex.ru>
2016-06-03 12:05:33 +0300

    початок

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <meta charset="utf-8" />
+  <!--подключение bootstrap-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link rel="stylesheet" href="/css/style.css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+  <!--подключение header(шапки)-->
+<?php
+  require_once("header.php");
+?>
+<div id="wrapper">
+
+</div>
+<!--подключение footer(подвала)-->
+<?php
+  require_once("footer.php");
+?>
+<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
